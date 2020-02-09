a6828f503e0227e8daa159486fd9b049e2f82bde
shaheemMPM <shaheem.mpm@gmail.com>
2020-02-09 13:14:43 +0530

    Add Hardcoded loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { WindMillLoading } from 'react-loadingg';
+import WindMillLoading from './WindMillLoading';
 
 const Loading = (props) => {
     return ( 
-        <WindMillLoading color="#3B0D00" size="large"/>
+        <WindMillLoading color="#c4f2ff" size="large"/>
     );
 }
- 
+
 export default Loading;
